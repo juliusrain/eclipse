5e3191ee22509b367901f2130fac8a562aaf9780
Brandon Wong <projects@brwong.net>
2011-11-22 19:45:46 -0500

    basic UI input functionality
    added ability for event listeners triggering given functions accepting x, y
    coordinates to be attached to arbitrary DOM objects.

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,12 +1,29 @@
 // The Primary Game Object
 function Game() {
+	// get canvas elements and contexts
 	var cnvs = document.getElementById('maincanvas');
 	var ctx = cnvs.getContext('2d');
+	var minmap = document.getElementById('minimap');
+	var map = minmap.getContext('2d');
 	var player = Player(ctx);
-	setInterval(function () {
+	
+	/*setInterval(function () {
 		cnvs.width = cnvs.width;
 		player.draw();
-	}, 60);
-	var ui = UI(cnvs, window);
-}
+	}, 60);*/
+	var ui = UI({obj:cnvs, func:clickOnMain}, {obj:minmap, func:clickOnMinimap});
 
+	// what to do when
+	
+	function clickOnMain(x, y) {
+		console.log('click on main map, relative coords:');
+		console.log(x+"x"+y);
+	
+	}
+	
+	function clickOnMinimap(x, y) {
+		console.log('click on minimap, relative coords:');
+		console.log(x+"x"+y);
+		
+	}
+}
--- scripts/UI.js
+++ scripts/UI.js
@@ -1,14 +1,34 @@
 // user interactions class
 
 function UI() {
+	// Mouse Events
+	// 	Attaches an event listener to each argument object.
+	// 	Each event listener captures the location of the click and executes the given function.
 	for(var i in arguments){
-		console.log(arguments[i]);
-		arguments[i].onclick = function (e) {
-			console.log(this);
-			console.log(e);
-			console.log("page: "+e.pageX+"x"+e.pageY);
-			return false;
-		};
+		console.log("UI for: "+arguments[i].obj);
+		// argument must be defined, must contain an object and a function
+		if(typeof arguments[i] != "undefined" && typeof arguments[i].obj == "object" && typeof arguments[i].func == "function"){
+			// generate and attach event listener
+			arguments[i].obj.onclick = function (fn) {
+				return (function (e) {
+					var ox = typeof this.offsetLeft == "undefined" ? 0 : this.offsetLeft,
+						oy = typeof this.offsetTop == "undefined" ? 0 : this.offsetTop,
+						x = e.pageX - ox,
+						y = e.pageY - oy;
+					fn(x, y);
+				});
+			}(arguments[i].func);
+		}
 	}
-	console.log('UI class loaded.');
+	
+	// Keyboard Events
+	window.onkeypress = function (e){
+		console.log("key!");
+		console.log(e);
+		/*console.log("keyCode "+e.keyCode);
+		console.log("charCode "+e.charCode);
+		console.log("which "+e.which);*/
+	}
+	
+	console.log('UI suite loaded.');
 }
