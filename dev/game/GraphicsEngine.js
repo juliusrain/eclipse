a72602e0333731eafdbc0f0a3324412229062ae4
chris <chris@linnie.(none)>
2012-02-16 10:12:16 -0500

    dynamic partilces

--- dev/game/GraphicsEngine.js
+++ dev/game/GraphicsEngine.js
@@ -68,6 +68,20 @@ function GraphicsEngine() {
         gameCamera.aspect = (width/height);
         gameCamera.updateProjectionMatrix();
     }
+
+    var removed = false;
+
+    document.addEventListener('mousedown', onMouseDown, false);
+
+    function onMouseDown(event) {
+        if(!removed) {
+            engine.removeSceneObject(sceneElements.AIShips[0]);
+            engine.addExplosion(-10, 0, 0);
+        } else {
+            engine.addSceneObject(sceneElements.AIShips[0]);
+        }
+        removed = !removed;
+    }
 ///////////////////////////////////////
 }
 
@@ -80,6 +94,8 @@ function GraphicsEngine() {
                 var sphereGeometry = new THREE.SphereGeometry(1,10,10);
                 var tempSphere1 = new THREE.Mesh(sphereGeometry, tempMaterial);
                     tempSphere2 = new THREE.Mesh(sphereGeometry, tempMaterial);
+                tempSphere1.name = "sphere1";
+                tempSphere2.name = "sphere2"
 
                 var infinityreverse = false;
                 var axishelper = new THREE.AxisHelper();
@@ -99,10 +115,10 @@ function GraphicsEngine() {
         //this.gameplay_scene.add(axishelper);
         this.gameplay_scene.add(tempSphere1);
         this.gameplay_scene.add(tempSphere2);
-        
 
-        
-        
+
+
+
 ////////////////////////////
 
 
@@ -299,39 +315,56 @@ function GraphicsEngine() {
     GraphicsEngine.prototype.addSceneObject = function(sceneObject) {
         this.gameplay_scene.add(sceneObject);
     }
-    
+
+    //NEED TO FIX THIS UP
     GraphicsEngine.prototype.addExplosion = function(x, y, z) { //plus other vars
         //will need multiple systems for the different textures
-        // var container = new THREE.Object3D();
-        // var geometry = new THREE.Geometry();
-        // var particlePosition, particleMaterial, particleSystem;
-        // var i;
-        // for(i = 0; i < 100; i++) {
-            // particlePosition = new THREE.Vector3(0, 0, 0);
-            // geometry.vertices.push(new THREE.Vertex(particlePosition));
-            // geometry.vertices[i].direction = new THREE.Vector3(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1);
-            // geometry.vertices[i].speed = Math.random() * 10;
-        // }
-        
-        // particleMaterial = new THREE.ParticleBasicMaterial({
-            // color: 0xff0000,
-            // size: 100,
-            
-        // });
-        
-        // particleSystem = new THREE.ParticleSystem(geometry, particleMaterial);
-        // //particleSystem.position.set(x, y, z);
-        // particleSystem.position.x = -10;
-        // particleSystem.name = "particle system";
-        
-        // this.gameplay_scene.add(particleSystem);
-        console.log("bfd");
-        var geometry = new THREE.SphereGeometry(20, 20, 20);
-        var mesh = new THREE.Mesh(geometry, tempMaterial);
-        mesh.position.set(-10, 0, 0);
-        mesh.name = "blah";
-        
-        this.gameplay_scene.add(mesh);
+        var container = new THREE.Object3D();
+        container.name = "ps";
+
+        var geometry1 = new THREE.Geometry();
+        var particleMaterial1, particleSystem1;
+        var particlePosition;
+        var i;
+        for(i = 0; i < 100; i++) {
+            particlePosition = new THREE.Vector3(0, 0, 0);
+            geometry1.vertices.push(new THREE.Vertex(particlePosition));
+            geometry1.vertices[i].direction = new THREE.Vector3(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1);
+            geometry1.vertices[i].speed = Math.random() * 10;
+        }
+
+        particleMaterial1 = new THREE.ParticleBasicMaterial({
+            color: 0xff0000,
+            size: 100,
+
+        });
+
+        var geometry2 = new THREE.Geometry();
+        var particleMaterial2, particleSystem2;
+
+        particleSystem1 = new THREE.ParticleSystem(geometry1, particleMaterial1);
+        particleSystem1.position.set(x, y, z);
+        particleSystem1.name = "particle system1";
+
+        container.add(particleSystem1);
+
+        for(i = 0; i < 50; i++) {
+            particlePosition = new THREE.Vector3(0, 0, 0);
+            geometry2.vertices.push(new THREE.Vertex(particlePosition));
+        }
+        particleMaterial2 = new THREE.ParticleBasicMaterial({
+            color: 0x0000ff,
+            size: 50
+        });
+
+        particleSystem2 = new THREE.ParticleSystem(geometry2, particleMaterial2);
+        particleSystem2.position.set(x+60, y, z);
+        particleSystem2.name = "particle system2";
+
+
+        container.add(particleSystem2);
+
+        this.gameplay_scene.add(container);
     }
 
 //=================================================================
@@ -370,7 +403,7 @@ function GraphicsEngine() {
 
             drawMainShip();
             drawHUD();
-            updateScene();
+            //updateScene();
 
             renderer.clear();
             renderer.render(gameScene, gameCamera); //actual game scene
