cfb4f51d5206354c40844635d1dfa5648d26e3d4
Brandon Wong <brandon.wong@mail.mcgill.ca>
2012-02-21 04:54:54 -0500

    modified styling

--- dev/game/GameEngine.js
+++ dev/game/GameEngine.js
@@ -3,7 +3,13 @@ function GameEngine() {
 	this.gameID;// = <?php echo $number; ?>;//get from game maker?
 	this.solarSystem;
 	this.planet;
-	this.resources = {};//load from game constants!
+	//this.resources = {};//load from game constants!
+	// TEMPORARY ***************************************
+	this.resources = {
+		food:1000,
+		fuel:1000,
+		metals:1000,
+	};
 	this.stage = 0;
 	// new game portion - NO NEED FOR THIS!
 	   // call database, create new game(?)
@@ -31,25 +37,35 @@ GameEngine.prototype.save = function () {
 
 // GameEngine load function
 // called when jumping into another system
-GameEngine.prototype.load = function (ssid, pid){
+GameEngine.prototype.load = function (ssid, pid) {
 	// wipe everything
-	//     *********clear
-	//     place loader screen
+	//     clear
+	$('#loader').show();
 	// determine winning or losing state
 	//     blah blah blah
-    this.solarSystem = ssid;
-    this.planet = pid;
 	// get information from network
-	var received = network.retrievePlanet(this.gameID, this.solarSystem, this.planet);
+	var received = network.retrievePlanet(this.gameID, ssid, pid);
     graphicsEngine.loadGamePlayObjects(received);
+	this.solarSystem = ssid;
+	this.planet = pid;
 	// trigger GraphicsEngine
     graphicsEngine.startEngine();
     // remove loader screen
 };
 
+GameEngine.prototype.updateResourcesBar = function () {
+	$('#food').html(this.resources.food);
+	$('#fuel').html(this.resources.fuel);
+	$('#metals').html(this.resources.metals);
+};
+
+GameEngine.prototype.updateResources = function () {
+	
+};
+
 // GameEngine update function
 // called each frame
-GameEngine.prototype.update = function (){
+GameEngine.prototype.update = function () {
 
 };
 
