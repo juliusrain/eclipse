310e2d175a31c2681c55e0d202337f8953a836b0
Christopher Wong <christopher.wong@mail.mcgill.ca>
2012-02-22 13:25:36 -0500

    working on texturing

--- dev/game/UI.js
+++ dev/game/UI.js
@@ -23,6 +23,12 @@ $(window).keypress(function (e){
     }
 });
 
+$('#chatcompose').keydown(function (e){
+    
+    $('#chatcompose').val($('#chatcompose').val() + String.fromCharCode(e.keyCode));
+    return false;
+});
+
 function toggleJumpMap() {
     if($('#jumpmapbox:visible').length){
         $('#jumpmapbox').hide();
