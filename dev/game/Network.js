364b7a6528f3dd803411f2270c3917a80a57985a
Saining Li <saining.li@gmail.com>
2012-03-10 14:37:06 -0500

    merge new network stuff

--- dev/game/Network.js
+++ dev/game/Network.js
@@ -1,12 +1,31 @@
 // Network Component
 
-URL = "ws://cgi.cs.mcgill.ca:8080/~sli90/ws"
-
 function Network() {
 	//this part will hold the network connection
-	this.ws = new WebSocket(URL);
+	this.ws;
+}
+
+Network.prototype.retrievePlanet = function (gid, ssid, pid) {
+	var received = gameObjects;
+	for(e in received){
+		if(received[e].type === SKYBOX){
+			for(var i = 0; i < 6; i++){
+				received[e].parameters.textures[i] = "textures/" + received[e].parameters.textures[i];
+				console.log(received[e].parameters.textures[i]);
+			}
+		}
+	}
+	return received;
+}
+
+Network.prototype.connect = function () {
+    this.ws = new WebSocket(URL);
+    this.ws.on_close() = function {
+        Network.prototype.connect;
+    }
 }
 
 Network.prototype.send = function (message) {
-    this.ws.send(message)
+    // takes a JSON object and sends it to server as string
+    this.ws.send(JSON.stringify(message))
 }
