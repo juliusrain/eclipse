dedcc44e9b8071534c34a87a08925e63ef208fe4
Saining Li <saining.li@gmail.com>
2012-03-09 21:18:04 -0500

    network refactor

--- dev/game/Network.js
+++ dev/game/Network.js
@@ -4,24 +4,7 @@ URL = "ws://cgi.cs.mcgill.ca:8080/~sli90/ws"
 
 function Network() {
 	//this part will hold the network connection
-	this.ws;
-}
-
-Network.prototype.retrievePlanet = function (gid, ssid, pid) {
-	var received = gameObjects;
-	for(e in received){
-		if(received[e].type === SKYBOX){
-			for(var i = 0; i < 6; i++){
-				received[e].parameters.textures[i] = "textures/" + received[e].parameters.textures[i];
-				console.log(received[e].parameters.textures[i]);
-			}
-		}
-	}
-	return received;
-}
-
-Network.prototype.connect = function () {
-    this.ws = new WebSocket(URL);
+	this.ws = new WebSocket(URL);
 }
 
 Network.prototype.send = function (message) {
