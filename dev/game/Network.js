77494ba8c5c4263e198c2badf0ea23a600ee8c69
Christopher Wong <christopher.wong@mail.mcgill.ca>
2012-03-10 15:57:10 -0500

    merged network

--- dev/game/Network.js
+++ dev/game/Network.js
@@ -5,6 +5,18 @@ function Network() {
 	this.ws;
 }
 
+Network.prototype.retrievePlanet = function (gid, ssid, pid) {
+	var received = gameObjects;
+	for(e in received){
+		if(received[e].type === SKYBOX){
+			for(var i = 0; i < 6; i++){
+				received[e].parameters.textures[i] = "textures/" + received[e].parameters.textures[i];
+				console.log(received[e].parameters.textures[i]);
+			}
+		}
+	}
+	return received;
+}
 
 Network.prototype.connect = function () {
     this.ws = new WebSocket(URL);
